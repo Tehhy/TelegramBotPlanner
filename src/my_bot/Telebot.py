9a81d6e3c4a669bfb941f1cda6f23576fb2d4e13
Alena Nakidkina <alena.nakidkina@gmail.com>
2026-03-10 13:05:49 +0000

    pre-commit is added to dev dependencies, instructions added to readme file pre-commit is added to dev dependencies, instructions added to readme file

--- src/my_bot/Telebot.py
+++ src/my_bot/Telebot.py
@@ -1,13 +1,20 @@
 import os
-import telebot
 import random
+
+import telebot
 
 token = os.environ.get("TELEGRAM_TOKEN")
 bot = telebot.TeleBot(token)
 
-RANDOM_TASKS = ["Read a chapter of Ulysses @Reading", "Compliment a stranger @Crazy",
-                "Give away some clothes @Karma", "Pick up litter in the neighbourhood @Karma",
-                "Fix the damn door @HomeSweetHome", "Dance party with kids @Kids", "Wash the car @Car"]
+RANDOM_TASKS = [
+    "Read a chapter of Ulysses @Reading",
+    "Compliment a stranger @Crazy",
+    "Give away some clothes @Karma",
+    "Pick up litter in the neighbourhood @Karma",
+    "Fix the damn door @HomeSweetHome",
+    "Dance party with kids @Kids",
+    "Wash the car @Car",
+]
 
 HELP = """
 /help - list available commands.
@@ -17,22 +24,28 @@ HELP = """
 
 tasks = {}
 
+
 def add_todo(date, task):
     if date in tasks:
         tasks[date].append(task)
     else:
         tasks[date] = [task]
 
+
 @bot.message_handler(commands=["help"])
 def help(message):
     bot.send_message(message.chat.id, HELP)
+
 
 @bot.message_handler(commands=["add", "todo"])
 def add(message):
     try:
         command = message.text.split(maxsplit=1)
         if len(command) < 2:
-            bot.send_message(message.chat.id, "Invalid command format. Use: /add <date> <task> @<category>")
+            bot.send_message(
+                message.chat.id,
+                "Invalid command format. Use: /add <date> <task> @<category>",
+            )
             return
         date = command[1].split()[0].lower()
         task_with_category = command[1].split(maxsplit=1)[1]
@@ -44,9 +57,15 @@ def add(message):
             task = task_with_category.strip()
             category = ""
         add_todo(date, task + " " + category)
-        bot.send_message(message.chat.id, f"Task '{task} {category}' added on date {date}")
+        bot.send_message(
+            message.chat.id, f"Task '{task} {category}' added on date {date}"
+        )
     except IndexError:
-        bot.send_message(message.chat.id, "Invalid command format. Use: /add <date> <task> @<category>")
+        bot.send_message(
+            message.chat.id,
+            "Invalid command format. Use: /add <date> <task> @<category>",
+        )
+
 
 @bot.message_handler(commands=["random"])
 def random_add(message):
@@ -55,11 +74,14 @@ def random_add(message):
     add_todo(date, task)
     bot.send_message(message.chat.id, f"Task '{task}' added on date {date}")
 
+
 @bot.message_handler(commands=["show", "print"])
 def show(message):
     command = message.text.split()
     if len(command) < 2:
-        bot.send_message(message.chat.id, "Invalid command format. Use: /show <date1> <date2> ...")
+        bot.send_message(
+            message.chat.id, "Invalid command format. Use: /show <date1> <date2> ..."
+        )
         return
     dates = [date.lower() for date in command[1:]]
     result = ""
@@ -71,4 +93,3 @@ def show(message):
         else:
             result += f"There are no tasks for date {date}\n"
     bot.send_message(message.chat.id, result)
-
